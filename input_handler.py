f48a1939e65172f15add6bbdb9c6418176f52055
poubr <46137915+poubr@users.noreply.github.com>
2024-06-14 23:53:06 +0300

    Continue with user input validation

--- input_handler.py
+++ input_handler.py
@@ -26,11 +26,10 @@ def get_user_input() -> None:
     if session_length == 0:
         return
 
-    print("Next, what operations would you like to include? Type Y or N to all that apply:")
-    addition = input("Addition Y/N: ")
-    subtraction = input("Subtraction Y/N: ")
-    multiplication = input("Multiplication Y/N: ")
-    division = input("Division Y/N: ")
+    list_of_operations = get_operations_list()
+    if not list_of_operations:
+        return
+
     print("Got it! Finally, choose your difficulty: Beginner, Intermediate, or Expert?")
     difficult = input("Type 'B', 'I', or 'E': ")
     print("Now let's practice!")
@@ -74,10 +73,10 @@ def get_user_input() -> None:
 
 def get_session_length() -> int:
     """Retrieves session length from user input. Asks once before offering option to quit."""
-    for i in range(0, 7):
+    for i in range(3):
         prompt = "Enter 5, 10, or 15: "
         if i > 0:
-            prompt = f"{prompt[:-2]}, or 'Q to quit:"
+            prompt = f"{prompt[:-2]}, or 'Q to quit: "
         user_input = input(prompt)
         if user_input in ["5", "10", "15"]:
             return int(user_input)
@@ -85,6 +84,33 @@ def get_session_length() -> int:
             print("Ending session.")
             return 0
         else:
-            print("Please insert a valid answer")
+            print("Please insert a valid answer.")
     return 0
 
+
+def get_operations_list() -> list:
+    """Retrieves the list of operations from user input."""
+    print("Next, what operations would you like to include? Type 'Y' or 'N' to all that apply:")
+    available_operations = ["Addition", "Subtraction", "Multiplication", "Division"]
+    user_operations = []
+
+    for operation in available_operations:
+        for i in range(3):
+            prompt = f"{operation} Y/N: "
+            if i > 0:
+                prompt = f"Type 'N' to choose {operation.lower()}, 'N' to skip {operation.lower()}, or 'q' to quit: "
+            user_input = input(prompt)
+            if user_input.lower() == "y":
+                user_operations.append(operation)
+                break
+            elif user_input.lower() == "n":
+                break
+            elif i > 0 and user_input.lower() == "q":
+                print("Ending session.")
+                return []
+            else:
+                print("Please insert a valid answer.")
+    if not user_operations:
+        print("You haven't selected anything to practice! Exiting...")
+
+    return user_operations
