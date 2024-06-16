139e2ad16dc3d97861406d456c86f8f02ba58d02
poubr <46137915+poubr@users.noreply.github.com>
2024-06-16 14:25:26 +0300

    Fix bug in prompting valid answer in input validation

--- input_handler.py
+++ input_handler.py
@@ -31,7 +31,7 @@ def get_user_input() -> None:
         return
 
     print("Got it! Finally, choose your difficulty: Beginner, Intermediate, or Expert?")
-    difficult = input("Type 'B', 'I', or 'E': ")
+    difficulty = input("Type 'B', 'I', or 'E': ")
     print("Now let's practice!")
     sleep(1)
     print("Sorry, no math coded yet. Here's a cookie instead:")
@@ -73,7 +73,7 @@ def get_user_input() -> None:
 
 def get_session_length() -> int:
     """Retrieves session length from user input. Asks once before offering option to quit."""
-    for i in range(3):
+    for i in range(4):
         prompt = "Enter 5, 10, or 15: "
         if i > 0:
             prompt = f"{prompt[:-2]}, or 'Q to quit: "
@@ -83,6 +83,8 @@ def get_session_length() -> int:
         elif i > 0 and user_input.lower() == "q":
             print("Ending session.")
             return 0
+        elif i == 3:
+            print("It seems you don't want to practice math today! Exiting...")
         else:
             print("Please insert a valid answer.")
     return 0
@@ -95,7 +97,7 @@ def get_operations_list() -> list:
     user_operations = []
 
     for operation in available_operations:
-        for i in range(3):
+        for i in range(4):
             prompt = f"{operation} Y/N: "
             if i > 0:
                 prompt = f"Type 'N' to choose {operation.lower()}, 'N' to skip {operation.lower()}, or 'q' to quit: "
@@ -103,7 +105,8 @@ def get_operations_list() -> list:
             if user_input.lower() == "y":
                 user_operations.append(operation)
                 break
-            elif user_input.lower() == "n":
+            elif user_input.lower() == "n" or i == 3:
+                print(f"Skipping {operation.lower()}.")
                 break
             elif i > 0 and user_input.lower() == "q":
                 print("Ending session.")
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from input_handler import get_user_input
 
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     get_user_input()
