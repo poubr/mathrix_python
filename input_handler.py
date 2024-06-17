ec6e2301a20916870f472faa45e8246e790a3e08
poubr <46137915+poubr@users.noreply.github.com>
2024-06-17 23:13:00 +0300

    Add converting of input, improve returns

--- input_handler.py
+++ input_handler.py
@@ -3,7 +3,7 @@
 from time import sleep
 
 
-def get_user_input() -> None:
+def get_user_input() -> dict:
     """Retrieves the configs for the practice session via user input."""
 
     print("Hello and welcome to ....")
@@ -24,15 +24,21 @@ def get_user_input() -> None:
 
     session_length = get_session_length()
     if session_length == 0:
-        return
+        return {}
 
     list_of_operations = get_operations_list()
     if not list_of_operations:
-        return
+        return {}
 
     difficulty = get_session_difficulty()
-    if not difficulty:
-        return
+    if difficulty == 0:
+        return {}
+
+    user_configuration = {
+        "session_length": session_length,
+        "list_of_operations": list_of_operations,
+        "difficulty": difficulty
+    }
 
     print("Now let's practice!")
     sleep(1)
@@ -72,6 +78,8 @@ def get_user_input() -> None:
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⡿⣿⡟⠻⢿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣿⣿⠿⠛⠛⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
     ''')
 
+    return user_configuration
+
 
 def get_session_length() -> int:
     """Retrieves session length from user input. Asks once before offering option to quit."""
@@ -105,7 +113,7 @@ def get_operations_list() -> list:
                 prompt = f"Type 'N' to choose {operation.lower()}, 'N' to skip {operation.lower()}, or 'q' to quit: "
             user_input = input(prompt)
             if user_input.lower() == "y":
-                user_operations.append(operation)
+                user_operations.append(operation.lower())
                 break
             elif user_input.lower() == "n" or i == 3:
                 print(f"Skipping {operation.lower()}.")
@@ -121,8 +129,8 @@ def get_operations_list() -> list:
     return user_operations
 
 
-def get_session_difficulty() -> str:
-    """Retreives session difficulty from user input, returns an empty string if answer was not provided."""
+def get_session_difficulty() -> int:
+    """Retrieves session difficulty from user input, returns an empty string if answer was not provided."""
     print("Got it! Finally, choose your difficulty: Beginner, Intermediate, or Expert?")
 
     for i in range(4):
@@ -131,12 +139,28 @@ def get_session_difficulty() -> str:
             prompt = f"{prompt[:-2]}, or 'Q to quit: "
         user_input = input(prompt)
         if user_input.lower() in ["b", "i", "e"]:
-            return user_input.lower()
+            difficulty = convert_difficulty_input(user_input.lower())
+            return difficulty
         elif i > 0 and user_input.lower() == "q":
             print("Ending session.")
-            return ""
+            return 0
         elif i == 3:
             print("It seems you don't want to practice math today! Exiting...")
         else:
             print("Please insert a valid answer.")
-    return ""
+    return 0
+
+
+def convert_difficulty_input(user_input: str) -> int:
+    """Converts the alphabetic input to a corresponding integer.
+    'b' (beginner) == calculations within 20
+    'i' (intermediate) == calculations within 100
+    'e' (expert) == calculations within 1000
+    """
+    input_mapping = {
+        "b": 20,
+        "i": 100,
+        "e": 1000
+    }
+
+    return input_mapping[user_input]
--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from input_handler import get_user_input
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    get_user_input()
+    user_configuration = get_user_input()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
